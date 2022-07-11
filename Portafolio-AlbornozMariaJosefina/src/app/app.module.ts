18c7e21c798de864ad0a586c30cad75772017bbc
Josefina Albornoz <josefinaalbornoz26@gmail.com>
2022-07-11 04:05:27 -0300

    Se agrega habilidades
    falta modificar el espacio de cada barra circular (separar entre si), agregar imágenes y porcentajes -vinculo con base de datos-

--- Portafolio-AlbornozMariaJosefina/src/app/app.module.ts
+++ Portafolio-AlbornozMariaJosefina/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
 
 @NgModule({
   declarations: [
@@ -21,10 +23,12 @@ import { EducacionComponent } from './components/educacion/educacion.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
+    HabilidadesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
